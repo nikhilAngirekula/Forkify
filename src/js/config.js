27685bf8fb7d550cb6b5415509b85e187d7ac54b
nikhil <nikhil.angirekula4@gmail.com>
2021-07-09 17:58:40 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '49d0a0d2-dca4-4914-9d6b-fd96174d7217';
+export const MODAL_CLOSE_SEC = 2.5;
