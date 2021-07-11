7e7fc30dc732c994b524a3fadb33ce13314dbd02
nikhil <nikhil.angirekula04>
2021-07-11 09:43:21 +0530

    Corrected the console message

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,7 +120,7 @@ const controlAddRecipe = async function (newRecipe) {
 };
 
 const newFeature = function () {
-  console.log('Welcome to new feature');
+  console.log('Welcome to the application');
 };
 
 const init = function () {
